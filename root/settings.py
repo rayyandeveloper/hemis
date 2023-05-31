9d4efce2aa1852f224ebdfdf99a7843392924549
Abdurazzoq <rayyan.dev@mail.ru>
2023-05-31 09:59:52 +0500

    asdasd

--- root/settings.py
+++ root/settings.py
@@ -124,7 +124,7 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-print(DEBUG)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
